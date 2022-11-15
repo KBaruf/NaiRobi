f48623872df509da3fcf03213e632250d7294082
Baruf Kosgei <you@example.com>
2022-11-14 19:06:32 -0600

    build: remove webpack config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  // env: {
-  //   NEXTAUTH_SECRET: '2f1381d7ef038c0a901654c9ba38ea5e',
-  // },
 };
 
 module.exports = nextConfig;
