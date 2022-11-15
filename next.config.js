41ca9da3c83cc493f33e51d283757b0a5845dcbf
Baruf Kosgei <you@example.com>
2022-11-14 22:43:09 -0600

    build: separated dev key from prod key

--- next.config.js
+++ next.config.js
@@ -6,11 +6,22 @@ const nextConfig = {
 
 module.exports = nextConfig;
 
-module.exports = {
-  env: {
-    REACT_APP_AUTH_STRIPE_PUBLIC: 'pk_test_Mm7nkve67POBdco4a7x2AZKj',
-    REACT_APP_AUTH_STRIPE_SECRET: 'sk_test_4XjfNBJK6chY8mT3A6wSCFVk',
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-    NEXT_PUBLIC_SECRET: '2f1381d7ef038c0a901654c9ba38ea5e',
-  },
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        REACT_APP_AUTH_STRIPE_PUBLIC: 'pk_test_Mm7nkve67POBdco4a7x2AZKj',
+        REACT_APP_AUTH_STRIPE_SECRET: 'sk_test_4XjfNBJK6chY8mT3A6wSCFVk',
+        NEXT_PUBLIC_SECRET: '2f1381d7ef038c0a901654c9ba38ea5y',
+      },
+    };
+  }
+
+  return {
+    env: {
+      NEXT_PUBLIC_SECRET: '2f1381d7ef038c0a901654c9ba38ea5e',
+    },
+  };
 };
